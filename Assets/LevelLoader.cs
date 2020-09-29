cf4d2e72ff071d9e100c1529d550ae4e6bb30752
Brandon Horlacher <bhorlacher@tangynt.com>
2020-09-28 22:43:35 -0600

    adding textures and models.

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float sceneTransitionSeconds = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadNextScene", sceneTransitionSeconds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
